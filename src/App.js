38731ac56070470c9c95e7652c353dbb922f5ad0
ChristianSouza12 <christianlmsouza@outlook.com>
2023-08-15 10:08:16 -0300

    subindo projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import  React, { useState, useRef } from "react"
+import { Container, H1, Image, Container2, InputLabel, Input, Button, User } from "./styles"
+import People from "./assets/conversa.svg"
+import Arrow from "./assets/arrow.svg"
+import Trash from "./assets/trash.svg"
+import axios from "axios"
+// JSX = HTML COM JS 
+const App = () => {
+  const inputName = useRef()
+  const inputAge = useRef()
 
-function App() {
+  // const users = []
+
+  const [users, setUsers] = useState([]);
+
+ 
+  async function addNewUser() {
+    const data = await axios.post("http://localhost:3001/users",
+      {
+        name: inputName.current.value,
+
+        age: inputAge.current.value
+      });
+    
+    console.log(data)
+  
+  }
+
+  function deleteUser(userId) {
+    const newUsers = users.filter(user => user.id !== userId)
+    setUsers(newUsers)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+
+      <Image alt="logo-image" src={People} />
+
+      <Container2>
+
+
+
+        <H1>Olá !</H1>
+
+        <InputLabel>Nome</InputLabel>
+
+        <Input ref={inputName} type="text" placeholder="Nome"></Input>
+
+        <InputLabel>Idade</InputLabel>
+        <Input ref={inputAge} type="number" placeholder="Idade"></Input>
+
+        <Button onClick={addNewUser}>Cadastrar<img alt="seta" src={Arrow}></img></Button>
+
+
+        <ul>{
+          users.map(user => (
+            <User key={user.id}>
+              <p>{user.name}</p> <p>{user.age}</p>
+              <button onClick={() => deleteUser(user.id)}><img src={Trash} alt="Lata" /></button>
+            </User>
+          ))
+
+
+        }
+        </ul>
+      </Container2>
+
+    </Container>
+
+
+  )
+
 }
 
-export default App;
+export default App
